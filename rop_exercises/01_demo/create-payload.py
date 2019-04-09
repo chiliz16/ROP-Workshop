88743779e3b8fca205d35a7d2b06edc6a19c10f9
chiliz <chiliz0x10@gmail.com>
2019-04-09 13:34:19 +0200

    restructured workshop to 64 bit

--- rop_exercises/01_demo/create-payload.py
+++ rop_exercises/01_demo/create-payload.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python2
 from pwn import *
 
-#STEP 1: find the right amount of bytes to overflow the buffer up to the saved return address 
+# STEP 1: find the right amount of bytes to overflow the buffer up to the saved return address
 FILLBUF = XXX
 
 # STEP 2: determine base address of libc and extract offsets to 'system' and '/bin/sh' 
-libcbase = XXX
-OFFSET_SYSTEM = XXX
+LIBC_BASE = XXX 
+OFFSET_SYSTEM = XXX 
 OFFSET_BIN_SH = XXX 
 
-# STEP 3: calculate addresses of 'system' and '/bin/sh'
-address_system = XXX 
-address_bin_sh = XXX
+# STEP 3: find ROP-Gadget to load a value into RDI 
+POP_RDI = XXX
 
-# STEP 4: Build ROP-Chain
+# STEP 4: calculate the addresses of 'system' and '/bin/sh'
+address_system = XXX 
+address_bin_sh = XXX 
+
+# STEP 5: build the ROP-chain
 payload = "A" * FILLBUF 
 payload += XXX 
+payload += XXX
 payload += XXX 
-payload += XXX 
+
 
 print payload
--- rop_exercises/02_demo/02_demo_64_bit.c
+++ rop_exercises/02_demo/02_demo_64_bit.c
@@ -7,7 +7,6 @@ int main()
 	int n = 0; 
 	printf("Give me the Code: \n");
 	n = read(STDIN_FILENO, data, 0x40);
-//	data[n] = 0;
 	printf("You gave me: %s", data);
 	return 0;	
 }
